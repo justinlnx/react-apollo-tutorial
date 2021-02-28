f78b8387331f7a6a100947ba71f5b2e3da345512
justinlnx <justinlnx@gmail.com>
2021-02-27 17:25:14 -0800

    query, mutation, routing, auth

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
+import { setContext } from '@apollo/client/link/context';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import { AUTH_TOKEN } from './constants';
 
 import {
   ApolloProvider,
@@ -15,15 +18,26 @@ import reportWebVitals from './reportWebVitals';
 const httpLink = createHttpLink({
   uri: 'http://localhost:4000',
 });
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem(AUTH_TOKEN);
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    }
+  }
+});
 const client = new ApolloClient({
-  link: httpLink,
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 })
 
 ReactDOM.render(
-  <ApolloProvider client={client}>
-    <App />
-  </ApolloProvider>,
+  <BrowserRouter>
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
